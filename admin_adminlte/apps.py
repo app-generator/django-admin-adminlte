9639aabda66840e534f16a55aa3de41851b2baaf
mominur-helios <mominur.rahman@heliosinsider.com>
2022-12-08 17:56:32 +0600

    added apage and authentication

---
+++ admin_adminlte/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AdminAdminlteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'admin_adminlte'
+    icon = 'fa fa-user'
