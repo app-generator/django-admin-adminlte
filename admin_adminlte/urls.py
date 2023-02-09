3e31772a809d2cb1a2d5b5e0326de7bfb66a40b2
imran-helios <imran.mridha@heliosinsider.com>
2023-02-09 16:28:13 +0600

    Lte theme

--- admin_adminlte/urls.py
+++ admin_adminlte/urls.py
@@ -8,10 +8,93 @@ urlpatterns = [
     path('dashboard-v2/', views.index2, name='dashboardv2'),
     path('dashboard-v3/', views.index3, name='dashboardv3'),
     path('widgets/', views.widgets, name='widgets'),
-    path('chartjs/', views.chartjs, name='chartjs'),
-    path('general-forms/', views.general_forms, name='general_forms'),
-    path('calendar/', views.calendar, name='calendar'),
     path('profile/', views.profile, name='profile'),
+
+    # Examples
+    path('examples/calendar/', views.examples_calendar, name='examples_calendar'),
+    path('examples/gallery/', views.examples_gallery, name='examples_gallery'),
+    path('examples/kanban/', views.examples_kanban, name='examples_kanban'),
+    path('examples/invoice/', views.examples_invoice, name='examples_invoice'),
+    path('invoice-print/', views.invoice_print, name='invoice_print'),
+    path('examples/profile/', views.examples_profile, name='examples_profile'),
+    path('examples/e-commerce/', views.examples_e_commerce, name='examples_e_commerce'),
+    path('examples/projects/', views.examples_projects, name='examples_projects'),
+    path('examples/project-add/', views.examples_project_add, name='examples_project_add'),
+    path('examples/project-edit/', views.examples_project_edit, name='examples_project_edit'),
+    path('examples/project-detail/', views.examples_project_detail, name='examples_project_detail'),
+    path('examples/contacts/', views.examples_contacts, name='examples_contacts'),
+    path('examples/faq/', views.examples_faq, name='examples_faq'),
+    path('examples/contact-us/', views.examples_contact_us, name='examples_contact_us'),
+
+    # Extra 
+    path('login-v1/', views.UserLoginViewV1.as_view(), name='login_v1'),
+    path('login-v2/', views.UserLoginViewV2.as_view(), name='login_v2'),
+    path('registration-v1/', views.register_v1, name='registration_v1'),
+    path('registration-v2/', views.register_v2, name='registration_v2'),
+    path('forgot-password-v1/', views.UserPasswordResetViewV1.as_view(), name='forgot_password_v1'),
+    path('forgot-password-v2/', views.UserPasswordResetViewV2.as_view(), name='forgot_password_v2'),
+    path('recover-password-v1/', views.UserPasswordChangeViewV1.as_view(), name='recover_password_v1'),
+    path('recover-password-v2/', views.UserPasswordChangeViewV2.as_view(), name='recover_password_v2'),
+    path('lockscreen/', views.lockscreen, name='lockscreen'),
+    path('legacy-user-menu/', views.legacy_user_menu, name='legacy_user_menu'),
+    path('language-menu/', views.language_menu, name='language_menu'),
+    path('error-404/', views.error_404, name='error_404'),
+    path('error-500/', views.error_500, name='error_500'),
+    path('pace/', views.pace, name='pace'),
+    path('blank-page/', views.blank_page, name='blank_page'),
+    path('starter-page/', views.starter_page, name='starter_page'),
+
+    # Search
+    path('search-simple/', views.search_simple, name='search_simple'),
+    path('search-enhanced/', views.search_enhanced, name='search_enhanced'),
+    path('simple-result/', views.simple_results, name='simple_results'),
+    path('enhanced-result/', views.enhanced_results, name='enhanced_results'),
+
+    path('iframe/', views.iframe, name='iframe'),
+
+
+
+    # Mailbox
+    path('mailbox/inbox/', views.mailbox_inbox, name='mailbox_inbox'),
+    path('mailbox/compose/', views.mailbox_compose, name='mailbox_compose'),
+    path('mailbox/read-mail/', views.mailbox_read_mail, name='mailbox_read_mail'),
+
+    # Charts
+    path('chartjs/', views.chartjs, name='chartjs'),
+    path('flot/', views.flot, name='flot'),
+    path('inline/', views.inline, name='inline'),
+    path('uplot/', views.uplot, name='uplot'),
+
+    # UI Elements
+    path('ui/general/', views.ui_general, name='ui_general'),
+    path('ui/icons/', views.ui_icons, name='ui_icons'),
+    path('ui/buttons/', views.ui_buttons, name='ui_buttons'),
+    path('ui/sliders/', views.ui_sliders, name='ui_sliders'),
+    path('ui/modals-alerts/', views.ui_modals_alerts, name='ui_modals_alerts'),
+    path('ui/navbar-tabs/', views.ui_navbar_tabs, name='ui_navbar_tabs'),
+    path('ui/timeline/', views.ui_timeline, name='ui_timeline'),
+    path('ui/ribbons/', views.ui_ribbons, name='ui_ribbons'),
+
+    # Forms
+     path('form/general/', views.form_general, name='form_general'),
+     path('form/advanced/', views.form_advanced, name='form_advanced'),
+     path('form/editors/', views.form_editors, name='form_editors'),
+     path('form/validation/', views.form_validation, name='form_validation'),
+
+     # Table
+     path('table/simple/', views.table_simple, name='table_simple'),
+     path('table/data/', views.table_data, name='table_data'),
+     path('table/jsgrid/', views.table_jsgrid, name='table_jsgrid'),
+
+    #Layouts
+    path('top-navigation/', views.top_navigation, name='top_navigation'),
+    path('top-nav-sidebar/', views.top_nav_sidebar, name='top_nav_sidebar'),
+    path('boxed/', views.boxed, name='boxed'),
+    path('fixed-sidebar/', views.fixed_sidebar, name='fixed_sidebar'),
+    path('fixed-sidebar-custom/', views.fixed_sidebar_custom, name='fixed_sidebar_custom'),
+    path('fixed-topnav/', views.fixed_topnav, name='fixed_topnav'),
+    path('fixed-footer/', views.fixed_footer, name='fixed_footer'),
+    path('collapsed-sidebar/', views.collapsed_sidebar, name='collapsed_sidebar'),
 
     # Authentication
     path('accounts/register/', views.register, name='register'),
--- admin_adminlte/views.py
+++ admin_adminlte/views.py
@@ -21,14 +21,62 @@ def register(request):
   
   context = {'form': form}
   return render(request, 'accounts/register.html', context)
+  
+def register_v1(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      print('Account created successfully!')
+      return redirect('/accounts/login/')
+    else:
+      print("Registration failed!")
+  else:
+    form = RegistrationForm()
+  
+  context = {'form': form}
+  return render(request, 'pages/examples/register.html', context)
+
+def register_v2(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      print('Account created successfully!')
+      return redirect('/accounts/login/')
+    else:
+      print("Registration failed!")
+  else:
+    form = RegistrationForm()
+  
+  context = {'form': form}
+  return render(request, 'pages/examples/register-v2.html', context)
 
 class UserLoginView(auth_views.LoginView):
   template_name = 'accounts/login.html'
   form_class = LoginForm
   success_url = '/'
 
+class UserLoginViewV1(auth_views.LoginView):
+  template_name = 'pages/examples/login.html'
+  form_class = LoginForm
+  success_url = '/'
+
+class UserLoginViewV2(auth_views.LoginView):
+  template_name = 'pages/examples/login-v2.html'
+  form_class = LoginForm
+  success_url = '/'
+
 class UserPasswordResetView(auth_views.PasswordResetView):
   template_name = 'accounts/forgot-password.html'
+  form_class = UserPasswordResetForm
+
+class UserPasswordResetViewV1(auth_views.PasswordResetView):
+  template_name = 'pages/examples/forgot-password.html'
+  form_class = UserPasswordResetForm
+
+class UserPasswordResetViewV2(auth_views.PasswordResetView):
+  template_name = 'pages/examples/forgot-password-v2.html'
   form_class = UserPasswordResetForm
 
 class UserPasswordResetConfirmView(auth_views.PasswordResetConfirmView):
@@ -37,6 +85,14 @@ class UserPasswordResetConfirmView(auth_views.PasswordResetConfirmView):
 
 class UserPasswordChangeView(auth_views.PasswordChangeView):
   template_name = 'accounts/password_change.html'
+  form_class = UserPasswordChangeForm
+
+class UserPasswordChangeViewV1(auth_views.PasswordChangeView):
+  template_name = 'pages/examples/recover-password.html'
+  form_class = UserPasswordChangeForm
+
+class UserPasswordChangeViewV2(auth_views.PasswordChangeView):
+  template_name = 'pages/examples/recover-password-v2.html'
   form_class = UserPasswordChangeForm
 
 def user_logout_view(request):
@@ -73,6 +129,285 @@ def widgets(request):
   }
   return render(request, 'pages/widgets.html', context)
 
+# EXAMPLES
+
+def examples_calendar(request):
+  context = {
+    'parent': '',
+    'segment': 'calendar'
+  }
+  return render(request, 'pages/calendar.html', context)
+
+def examples_gallery(request):
+  context = {
+    'parent': '',
+    'segment': 'gallery'
+  }
+  return render(request, 'pages/gallery.html', context)
+
+def examples_kanban(request):
+  context = {
+    'parent': '',
+    'segment': 'kanban_board'
+  }
+  return render(request, 'pages/kanban.html', context)
+
+# Mailbox
+
+def mailbox_inbox(request):
+  context = {
+    'parent': 'mailbox',
+    'segment': 'inbox'
+  }
+  return render(request, 'pages/mailbox/mailbox.html', context)
+
+def mailbox_compose(request):
+  context = {
+    'parent': 'mailbox',
+    'segment': 'compose'
+  }
+  return render(request, 'pages/mailbox/compose.html', context)
+
+def mailbox_read_mail(request):
+  context = {
+    'parent': 'mailbox',
+    'segment': 'read_mail'
+  }
+  return render(request, 'pages/mailbox/read-mail.html', context)
+
+# Pages -- Examples
+
+def examples_invoice(request):
+  context = {
+    'parent': 'pages',
+    'segment': 'invoice'
+  }
+  return render(request, 'pages/examples/invoice.html', context)
+
+def invoice_print(request):
+  context = {
+    'parent': 'pages',
+    'segment': 'invoice_print'
+  }
+  return render(request, 'pages/examples/invoice-print.html', context)
+
+def examples_profile(request):
+  context = {
+    'parent': 'pages',
+    'segment': 'profile'
+  }
+  return render(request, 'pages/examples/profile.html', context)
+
+def examples_e_commerce(request):
+  context = {
+    'parent': 'pages',
+    'segment': 'ecommerce'
+  }
+  return render(request, 'pages/examples/e-commerce.html', context)
+
+def examples_projects(request):
+  context = {
+    'parent': 'pages',
+    'segment': 'projects'
+  }
+  return render(request, 'pages/examples/projects.html', context)
+
+def examples_project_add(request):
+  context = {
+    'parent': 'pages',
+    'segment': 'project_add'
+  }
+  return render(request, 'pages/examples/project-add.html', context)
+
+def examples_project_edit(request):
+  context = {
+    'parent': 'pages',
+    'segment': 'project_edit'
+  }
+  return render(request, 'pages/examples/project-edit.html', context)
+
+def examples_project_detail(request):
+  context = {
+    'parent': 'pages',
+    'segment': 'project_detail'
+  }
+  return render(request, 'pages/examples/project-detail.html', context)
+
+def examples_contacts(request):
+  context = {
+    'parent': 'pages',
+    'segment': 'contacts'
+  }
+  return render(request, 'pages/examples/contacts.html', context)
+
+def examples_faq(request):
+  context = {
+    'parent': 'pages',
+    'segment': 'faq'
+  }
+  return render(request, 'pages/examples/faq.html', context)
+
+def examples_contact_us(request):
+  context = {
+    'parent': 'pages',
+    'segment': 'contact_us'
+  }
+  return render(request, 'pages/examples/contact-us.html', context)
+
+# Extra -- login & Registration v1
+# def login_v1(request):
+#   context = {
+#     'parent': '',
+#     'segment': ''
+#   }
+#   return render(request, 'pages/examples/login.html', context)
+
+# def login_v2(request):
+#   context = {
+#     'parent': '',
+#     'segment': ''
+#   }
+#   return render(request, 'pages/examples/login-v2.html', context)
+
+# def registration_v1(request):
+#   context = {
+#     'parent': '',
+#     'segment': ''
+#   }
+#   return render(request, 'pages/examples/register.html', context)
+
+# def registration_v2(request):
+#   context = {
+#     'parent': '',
+#     'segment': ''
+#   }
+#   return render(request, 'pages/examples/register-v2.html', context)
+
+# def forgot_password_v1(request):
+#   context = {
+#     'parent': '',
+#     'segment': ''
+#   }
+#   return render(request, 'pages/examples/forgot-password.html', context)
+
+# def forgot_password_v2(request):
+#   context = {
+#     'parent': '',
+#     'segment': ''
+#   }
+#   return render(request, 'pages/examples/forgot-password-v2.html', context)
+
+# def recover_password_v1(request):
+#   context = {
+#     'parent': '',
+#     'segment': ''
+#   }
+#   return render(request, 'pages/examples/recover-password.html', context)
+
+# def recover_password_v2(request):
+#   context = {
+#     'parent': '',
+#     'segment': ''
+#   }
+#   return render(request, 'pages/examples/recover-password-v2.html', context)
+
+def lockscreen(request):
+  context = {
+    'parent': '',
+    'segment': ''
+  }
+  return render(request, 'pages/examples/lockscreen.html', context)
+
+def legacy_user_menu(request):
+  context = {
+    'parent': 'extra',
+    'segment': 'legacy_user'
+  }
+  return render(request, 'pages/examples/legacy-user-menu.html', context)
+
+def language_menu(request):
+  context = {
+    'parent': 'extra',
+    'segment': 'legacy_menu'
+  }
+  return render(request, 'pages/examples/language-menu.html', context)
+
+def error_404(request):
+  context = {
+    'parent': 'extra',
+    'segment': 'error_404'
+  }
+  return render(request, 'pages/examples/404.html', context)
+
+def error_500(request):
+  context = {
+    'parent': 'extra',
+    'segment': 'error_500'
+  }
+  return render(request, 'pages/examples/500.html', context)
+
+def pace(request):
+  context = {
+    'parent': 'extra',
+    'segment': 'pace'
+  }
+  return render(request, 'pages/examples/pace.html', context)
+
+def blank_page(request):
+  context = {
+    'parent': 'extra',
+    'segment': 'blank_page'
+  }
+  return render(request, 'pages/examples/blank.html', context)
+
+def starter_page(request):
+  context = {
+    'parent': 'extra',
+    'segment': 'starter_page'
+  }
+  return render(request, 'pages/examples/starter.html', context)
+
+# Search
+
+def search_simple(request):
+  context = {
+    'parent': 'search',
+    'segment': 'search_simple'
+  }
+  return render(request, 'pages/search/simple.html', context)
+
+def search_enhanced(request):
+  context = {
+    'parent': 'search',
+    'segment': 'search_enhanced'
+  }
+  return render(request, 'pages/search/enhanced.html', context)
+
+def simple_results(request):
+  context = {
+    'parent': '',
+    'segment': ''
+  }
+  return render(request, 'pages/search/simple-results.html', context)
+
+def enhanced_results(request):
+  context = {
+    'parent': '',
+    'segment': ''
+  }
+  return render(request, 'pages/search/enhanced-results.html', context)
+
+# MISCELLANEOUS
+
+def iframe(request):
+  context = {
+    'parent': '',
+    'segment': ''
+  }
+  return render(request, 'pages/search/iframe.html', context)
+
+# Charts
+
 def chartjs(request):
   context = {
     'parent': 'charts',
@@ -80,19 +415,26 @@ def chartjs(request):
   }
   return render(request, 'pages/charts/chartjs.html', context)
 
-def general_forms(request):
+def flot(request):
   context = {
-    'parent': 'forms',
-    'segment': 'general_forms'
+    'parent': 'charts',
+    'segment': 'flot'
   }
-  return render(request, 'pages/forms/general.html', context)
+  return render(request, 'pages/charts/flot.html', context)
 
-def calendar(request):
+def inline(request):
   context = {
-    'parent': '',
-    'segment': 'calendar'
+    'parent': 'charts',
+    'segment': 'inline'
   }
-  return render(request, 'pages/calendar.html', context)
+  return render(request, 'pages/charts/inline.html', context)
+
+def uplot(request):
+  context = {
+    'parent': 'charts',
+    'segment': 'uplot'
+  }
+  return render(request, 'pages/charts/uplot.html', context)
 
 def profile(request):
   context = {
@@ -100,3 +442,171 @@ def profile(request):
     'segment': 'profile'
   }
   return render(request, 'pages/examples/profile.html', context)
+
+# Layout
+def top_navigation(request):
+  context = {
+    'parent': 'layout',
+    'segment': 'top_navigation'
+  }
+  return render(request, 'pages/layout/top-nav.html', context)
+
+def top_nav_sidebar(request):
+  context = {
+    'parent': 'layout',
+    'segment': 'top navigation with sidebar'
+  }
+  return render(request, 'pages/layout/top-nav-sidebar.html', context)
+
+def boxed(request):
+  context = {
+    'parent': 'layout',
+    'segment': 'boxed_layout'
+  }
+  return render(request, 'pages/layout/boxed.html', context)
+
+def fixed_sidebar(request):
+  context = {
+    'parent': 'layout',
+    'segment': 'fixed_layout'
+  }
+  return render(request, 'pages/layout/fixed-sidebar.html', context)
+
+def fixed_sidebar_custom(request):
+  context = {
+    'parent': 'layout',
+    'segment': 'layout_cuastom'
+  }
+  return render(request, 'pages/layout/fixed-sidebar-custom.html', context)
+
+def fixed_topnav(request):
+  context = {
+    'parent': 'layout',
+    'segment': 'fixed_topNav'
+  }
+  return render(request, 'pages/layout/fixed-topnav.html', context)
+
+def fixed_footer(request):
+  context = {
+    'parent': 'layout',
+    'segment': 'fixed_footer'
+  }
+  return render(request, 'pages/layout/fixed-footer.html', context)
+
+def collapsed_sidebar(request):
+  context = {
+    'parent': 'layout',
+    'segment': 'collapsed_sidebar'
+  }
+  return render(request, 'pages/layout/collapsed-sidebar.html', context)
+
+# UI Elements
+
+def ui_general(request):
+  context = {
+    'parent': 'ui',
+    'segment': 'general'
+  }
+  return render(request, 'pages/UI/general.html', context)
+
+def ui_icons(request):
+  context = {
+    'parent': 'ui',
+    'segment': 'icons'
+  }
+  return render(request, 'pages/UI/icons.html', context)
+
+def ui_buttons(request):
+  context = {
+    'parent': 'ui',
+    'segment': 'buttons'
+  }
+  return render(request, 'pages/UI/buttons.html', context)
+
+def ui_sliders(request):
+  context = {
+    'parent': 'ui',
+    'segment': 'sliders'
+  }
+  return render(request, 'pages/UI/sliders.html', context)
+
+def ui_modals_alerts(request):
+  context = {
+    'parent': 'ui',
+    'segment': 'modals_alerts'
+  }
+  return render(request, 'pages/UI/modals.html', context)
+
+def ui_navbar_tabs(request):
+  context = {
+    'parent': 'ui',
+    'segment': 'navbar_tabs'
+  }
+  return render(request, 'pages/UI/navbar.html', context)
+
+def ui_timeline(request):
+  context = {
+    'parent': 'ui',
+    'segment': 'timeline'
+  }
+  return render(request, 'pages/UI/timeline.html', context)
+
+def ui_ribbons(request):
+  context = {
+    'parent': 'ui',
+    'segment': 'ribbons'
+  }
+  return render(request, 'pages/UI/ribbons.html', context)
+
+# Forms
+
+def form_general(request):
+  context = {
+    'parent': 'forms',
+    'segment': 'form_general'
+  }
+  return render(request, 'pages/forms/general.html', context)
+
+def form_advanced(request):
+  context = {
+    'parent': 'forms',
+    'segment': 'advanced_form'
+  }
+  return render(request, 'pages/forms/advanced.html', context)
+
+def form_editors(request):
+  context = {
+    'parent': 'forms',
+    'segment': 'text_editors'
+  }
+  return render(request, 'pages/forms/editors.html', context)
+
+def form_validation(request):
+  context = {
+    'parent': 'forms',
+    'segment': 'validation'
+  }
+  return render(request, 'pages/forms/validation.html', context)
+
+# Table
+
+def table_simple(request):
+  context = {
+    'parent': 'tables',
+    'segment': 'simple_table'
+  }
+  return render(request, 'pages/tables/simple.html', context)
+
+def table_data(request):
+  context = {
+    'parent': 'tables',
+    'segment': 'data_table'
+  }
+  return render(request, 'pages/tables/data.html', context)
+
+def table_jsgrid(request):
+  context = {
+    'parent': 'tables',
+    'segment': 'jsGrid'
+  }
+  return render(request, 'pages/tables/jsgrid.html', context)
